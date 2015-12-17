6e08fa00f10d94fca37c8b9a741f54a30ad6c03f
Caio Carrara <ccarrara@thoughtworks.com>
2015-12-17 11:52:56 -0200

    Adiciona plugin para gerar arquivo de tags

---
+++ _plugins/generate_tags_file.rb
@@ -0,0 +1,30 @@
+require 'json'
+
+module Jekyll
+  class TagsFileGenerator < Generator
+    priority :lowest
+
+    def generate(site)
+      tags = Hash.new
+
+      site.posts.docs.each do |post|
+        post['tags'].each do |tag|
+          key = tag.values.first.downcase
+          value = post.data['title']
+
+          if tags[key].class == Array
+            tags[key] << value
+          else
+            tags[key] = [value]
+          end
+        end
+      end
+
+      File.open('tags.json','w') do |f|
+        f.write(tags.to_json)
+      end
+
+      site.static_files << Jekyll::StaticFile.new(site,site.source,'','tags.json')
+    end
+  end
+end
